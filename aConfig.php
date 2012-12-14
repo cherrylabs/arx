b757dc42b56b6cb49743b94317b902ec006e0869
Daniel Sum <daniel@lacerisenumerique.be>
2012-12-14 12:05:32 +0100

    CLEANING

--- aConfig.php
+++ aConfig.php
@@ -12,15 +12,17 @@
 // @todo:
 // - clean system settings
 
-global $_config;
+global $aConfig;
 
-$_config = array(
+$aConfig = array(
 
     // System
-    'orm' => 'redbean',   // ORM used
-    'tpl' => 'savant3',   // template engine used
-    'route' => 'slim',    // routing system used
-    //'acl' => 'c_acl',
+    'system' => array(
+        'orm' => 'redbean',   // ORM used
+        'tpl' => 'savant3',   // template engine used
+        'route' => 'slim',    // routing system used
+        //'acl' => 'c_acl',
+    ),
 
     // Database
     'db_type' => 'sqlite', // mysql | sqlite
@@ -63,24 +65,24 @@ $_config = array(
         'scope' => 'email,publish_stream,read_friendlists,user_birthday'
     ),
 
-); // $_config
+); // $aConfig
 
 
 define('ZE_LANG', 'en'); // default language
-define('ZE_LANGS' , json_encode($_config['langs']));
+define('ZE_LANGS' , json_encode($aConfig['langs']));
 
 define('ZE_ADMINLOGIN', 'admin');
 define('ZE_SALT', 'hp[2d`I2+Z>[5l]@)q`.vc^X[DUcPIH8gY07#R}DL)L+NjwJ(1q0%C/!C)lpjc,T'); // https://api.wordpress.org/secret-key/1.1/salt/
 define('ZE_ALGO', 'sha1'); //sha1, sha256, md5 allowed
 define('ZE_ADMINPWD', 'admin');
 
-define('ZE_DBNAME', $_config['db_name']);
-define('ZE_DBUSER', $_config['db_user']);
-define('ZE_DBPASSWORD', $_config['db_pass']);
-define('ZE_DBHOST', $_config['db_host']);
-define('ZE_DBCHARSET', $_config['db_charset']);
-define('ZE_DBTYPE', $_config['db_type']);
-define('ZE_DBPREFIX', $_config['db_prefix']);
+define('ZE_DBNAME', $aConfig['db_name']);
+define('ZE_DBUSER', $aConfig['db_user']);
+define('ZE_DBPASSWORD', $aConfig['db_pass']);
+define('ZE_DBHOST', $aConfig['db_host']);
+define('ZE_DBCHARSET', $aConfig['db_charset']);
+define('ZE_DBTYPE', $aConfig['db_type']);
+define('ZE_DBPREFIX', $aConfig['db_prefix']);
 
 
 
@@ -96,8 +98,8 @@ if (preg_match( '/loc/' , $_SERVER['SERVER_NAME'])) {
 
     error_reporting(E_ALL & ~E_NOTICE);
 
-    $_config['facebook']['app_id'] = '';
-    $_config['facebook']['app_secret'] = '';
+    $aConfig['facebook']['app_id'] = '';
+    $aConfig['facebook']['app_secret'] = '';
 
 } elseif (preg_match( '/dev/' , $_SERVER['SERVER_NAME'] )) {
 
@@ -109,8 +111,8 @@ if (preg_match( '/loc/' , $_SERVER['SERVER_NAME'])) {
     
     error_reporting(E_ALL & ~E_NOTICE);
     
-    $_config['facebook']['app_id'] = '';
-    $_config['facebook']['app_secret'] = '';
+    $aConfig['facebook']['app_id'] = '';
+    $aConfig['facebook']['app_secret'] = '';
 
 } elseif (preg_match( '/demo/' , $_SERVER['SERVER_NAME'] )) {
 
@@ -120,8 +122,8 @@ if (preg_match( '/loc/' , $_SERVER['SERVER_NAME'])) {
     ini_set('display_errors', 1);
     ini_set('log_errors', 1);
 
-    $_config['facebook']['app_id'] = '206342166168916';
-    $_config['facebook']['app_secret'] = '42afbb4078f80b0c81ef6fc00261a72b';
+    $aConfig['facebook']['app_id'] = '206342166168916';
+    $aConfig['facebook']['app_secret'] = '42afbb4078f80b0c81ef6fc00261a72b';
 
 } else {
 
@@ -131,8 +133,8 @@ if (preg_match( '/loc/' , $_SERVER['SERVER_NAME'])) {
     ini_set('display_errors', 1);
     ini_set('log_errors', 1);
     
-    $_config['facebook']['app_id'] = '';
-    $_config['facebook']['app_secret'] = '';
+    $aConfig['facebook']['app_id'] = '';
+    $aConfig['facebook']['app_secret'] = '';
 
 }
 
